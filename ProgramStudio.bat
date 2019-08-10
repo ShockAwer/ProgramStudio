cd597d906ae9fa975dcc69c45c1bd961597d9ff7
zdrmonster1 <37600325+guest123guest@users.noreply.github.com>
2019-08-10 11:32:13 -0700

    Update ProgramStudio.bat

--- ProgramStudio.bat
+++ ProgramStudio.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFClbTx2+GGStCLkT6ezo0/qVrUoPVe8sbJrf07uxKeMc5HrcYZEAwnxlyp5aHAMWdxGkDg==
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdFe5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJhZksaHErSXA==
-::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbCXqZg==
-::ZQ05rAF9IAHYFVzEqQIAOhRTXgWPD3+vArwTiA==
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQIYKRVQQB3COW+zEvUe6fz456qBq0EYR6I2ds/amraPNOMbp0P2fJMj0nZD2N8DAxcZHg==
-::dhA7uBVwLU+EWHSN91A/OxNVShDi
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATEvGF5aDZdTxaNL2O7AKF8
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFClbTx2+GGStCLkT6ezo09qVrUoPVe8MbJrf07vAJfgWig==
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 title ProgramStudio
 color 87
