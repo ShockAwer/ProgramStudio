16157c5ff3c351ddbb9af732c08afafa5260c744
zdrmonster1 <37600325+guest123guest@users.noreply.github.com>
2019-08-10 11:44:10 -0700

    Update ProgramStudio.bat

--- ProgramStudio.bat
+++ ProgramStudio.bat
@@ -1,13 +1,11 @@
 @echo off
-title ProgramStudio NT 5.0
+title ProgramStudio 2000
 color 87
 rem Start the program
 
-echo ProgramStudio NT is starting!
+echo ProgramStudio 2000 is starting!
 ping -n 8 -w 1 192.168.254.254 >nul
 cls
-echo Sorry, i had to find a bunch of tutorials that don't work so i eventually gave up. Go into Notepad, and then click File, Save As, and change txt to All Files and save it under (yourprogram).bat
-pause
 SET /P _title= What is it called.
 echo Before we start, i recommend looking at quick tutorials on Batch before using, and don't  forget to use command "done" when finished.
 pause
