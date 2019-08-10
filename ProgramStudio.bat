edd3407d4930ce69a349628d2fd827f653df3758
zdrmonster1 <37600325+guest123guest@users.noreply.github.com>
2019-08-10 11:40:12 -0700

    Update ProgramStudio.bat

--- ProgramStudio.bat
+++ ProgramStudio.bat
@@ -1,9 +1,9 @@
 @echo off
-title ProgramStudio
+title ProgramStudio NT 5.0
 color 87
 rem Start the program
 
-echo ProgramStudio is starting!
+echo ProgramStudio NT is starting!
 ping -n 8 -w 1 192.168.254.254 >nul
 cls
 echo Sorry, i had to find a bunch of tutorials that don't work so i eventually gave up. Go into Notepad, and then click File, Save As, and change txt to All Files and save it under (yourprogram).bat
